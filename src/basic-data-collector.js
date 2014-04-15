697085c266e8dbc84296ec917bcbd7d92cf46899
Cain Wong <countcainht@gmail.com>
2014-04-15 15:56:27 +0800

    finish browser detection

--- src/basic-data-collector.js
+++ src/basic-data-collector.js
@@ -7,7 +7,17 @@
   exports.collector.browser = (function(){
     var browser = {};
     browser.language = navigator.language || navigator.userLanguage || navigator.systemLanguage;
-    browser.type = navigator.userAgent;
+    browser.type = (function(){
+      var ua = navigator.userAgent, tem,
+          M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*([\d\.]+)/i) || [];
+      if(/trident/i.test(M[1])){
+        tem =  /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
+        return 'IE '+ (tem[1] || '');
+      }
+      M = M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
+      if((tem = ua.match(/version\/([\.\d]+)/i)) !== null) M[2] = tem[1];
+      return M.join(' ');
+    })();
     return browser;
   })();
 
